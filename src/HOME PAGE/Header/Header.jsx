1acdb5ba78d2759561e66d690418a43465f2b715
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2023-01-31 09:42:41 +0100

    recent change

--- src/HOME PAGE/Header/Header.jsx
+++ src/HOME PAGE/Header/Header.jsx
@@ -12,6 +12,7 @@ import { useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../../Components/ContexApi/Contex'
 import { useSelector } from 'react-redux';
 function Header() {
+  const [search, setSearch] = useState(false)
   const [mobile, setMobile] = useState(true)
   const [category, setCategory] = useState(false)
   const [mobileCategory, setmobilCategory] = useState(false)
@@ -24,17 +25,70 @@ function Header() {
     return QTY
   }
   return (
-    <header className='header'>
+    <header className='header' >
       <div className='header1'>
         <div className='header1_wrap'>
+          <img className='pointer mobile_logo' onClick={() => Navigate('/')} style={{ width: 80 }} src={Unionheader} />
+          {search ? <div className='mobile_input'>
+              <input />
+              <BsSearch className='pointer' onClick={()=>setSearch(!search)} fontSize={13}/>
+            </div> : <div className='mobile_nav'>
+            <BsSearch onClick={()=>setSearch(!search)} className='pointer' />
+            <TbUserCircle className='pointer adm' onClick={() => { Navigate('/dashboard') }} fontSize={25} />
+            <span className='mobile_cart' onClick={() => Navigate('/cart')} >
+              <p >Cart</p>
+              <HiOutlineShoppingCart className='pointer adm' />{cart.length !== 0 ? <sup>{quantity()}</sup> : null}
+            </span>
+          </div>}
 
+
+          <nav className='hi_2' >
+            <img className='pointer' onClick={() => Navigate('/')} style={{ width: 70 }} src={Logo} />
+            <div className='input'>
+              <input />
+              <BsSearch className='pointer' />
+            </div>
+          </nav>
+
+          <nav className='hl_2'>
+            <TbUserCircle className='pointer adm' onClick={() => { Navigate('/dashboard') }} fontSize={30} />
+            <p onClick={() => { Navigate('/login'); }} >Login</p>
+            <p onClick={() => { Navigate('/signup'); }} >Sign up</p>
+            <span className='pointer adm' onClick={() => Navigate('/cart')} style={{ display: 'flex' }}>
+              <p >Cart</p>
+              <HiOutlineShoppingCart className='pointer adm' />{cart.length !== 0 ? <sup>{quantity()}</sup> : null}
+            </span>
+          </nav>
+
+        </div>
+      </div>
+
+      <div className='header2' onClick={()=>setSearch(false)}>
+        <div className='header2_wrap'>
+          <div
+            onMouseEnter={() => setCategory(true)}
+            onMouseLeave={() => setCategory(false)}
+            onClick={() => Navigate('/Catogories')} className='catigories pointer'>
+            <FiMenu fontSize={30} />
+            <p>All category</p>
+          </div>
+
+          {category && <div onMouseEnter={() => setCategory(true)} onMouseLeave={() => setCategory(false)} className='categories'>
+            <p>beds.</p>
+            <p>cabinets.</p>
+            <p>chairs and seating.</p>
+            <p>chests.</p>
+            <p>desks.</p>
+            <p>tables.</p>
+          </div>}
+          {!mobile && <div></div>}
           {mobile ? <FiMenu onClick={() => setMobile(!mobile)} className='mobile_menu' fontSize={25} /> :
             <div className='mobile_sidebar_cont'>
               <div className='mobile_sidebar'>
                 <div className='mobile_sidebar_close'>
                   <div className='mobile_sidebar_close_wrap '>
                     <div></div>
-                    <TbUserCircle className='pointer adm' onClick={() => { Navigate('/dashboard') }} fontSize={30} />
+                    <TbUserCircle className='pointer adm ' onClick={() => { Navigate('/dashboard') }} fontSize={30} />
                   </div>
 
                 </div>
@@ -56,54 +110,8 @@ function Header() {
                 </div>
               </div>
               <div onClick={() => setMobile(!mobile)} className='mobile_sidebar_invisible'></div>
-            </div>
-
-          }
-
-          <nav className='hi_2' >
-            <img className='pointer' onClick={() => Navigate('/')} style={{ width: 70 }} src={Logo} />
-            <div className='input'>
-              <input />
-              <BsSearch className='pointer' />
-            </div>
-          </nav>
-
-          <nav className='hl_2'>
-            <TbUserCircle className='pointer adm' onClick={() => { Navigate('/dashboard') }} fontSize={30} />
-            <p onClick={() => { Navigate('/login'); }} >Login</p>
-            <p onClick={() => { Navigate('/signup'); }} >Sign up</p>
-            <span className='pointer adm' onClick={() => Navigate('/cart')} style={{ display: 'flex' }}>
-              <p >Cart</p>
-              <HiOutlineShoppingCart className='pointer adm' />{cart.length !== 0 ? <sup>{quantity()}</sup> : null}
-            </span>
-          </nav>
-        </div>
-      </div>
-
-      <div className='header2'>
-        <div className='header2_wrap'>
-          <div
-            onMouseEnter={() => setCategory(true)}
-            onMouseLeave={() => setCategory(false)}
-            onClick={() => Navigate('/Catogories')} className='catigories pointer'>
-            <FiMenu fontSize={30} />
-            <p>All category</p>
-          </div>
-
-          {category && <div onMouseEnter={() => setCategory(true)} onMouseLeave={() => setCategory(false)} className='categories'>
-            <p>beds.</p>
-            <p>cabinets.</p>
-            <p>chairs and seating.</p>
-            <p>chests.</p>
-            <p>desks.</p>
-            <p>tables.</p>
-          </div>}
-          <img className='pointer mobile_logo' onClick={() => Navigate('/')} style={{ width: 80 }} src={Unionheader} />
+            </div>}
           <h3 style={{ color: '#003F62' }}>30 Days Free return</h3>
-          <span className='mobile_cart' onClick={() => Navigate('/cart')} style={{ display: 'flex' }}>
-              <p >Cart</p>
-              <HiOutlineShoppingCart className='pointer adm' />{cart.length !== 0 ? <sup>{quantity()}</sup> : null}
-            </span>
         </div>
       </div>
 
