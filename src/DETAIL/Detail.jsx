dcc922ac57c1958d21b320415714294b9b3698ef
oparah nkume nwachukwu <103194129+nwachukwuoparah@users.noreply.github.com>
2023-03-06 18:27:14 +0100

    order routh

--- src/DETAIL/Detail.jsx
+++ src/DETAIL/Detail.jsx
@@ -91,7 +91,7 @@ function Detail({ }) {
           </div>
         </div>
       </div>
-      {/* {recent?.length !== 0 ? <Products length={true} item={recent} loading={loading} title='Recently Viewed' /> : null} */}
+      {recent ? <Products length={true} item={recent} loading={loading} title='Recently Viewed' /> : null}
       <Products length={true} loading={loading} item={item1} title='Related items' />
     </div>
   )
